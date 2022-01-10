cdfe748ef9318eaa7533c416e0769d4e4311456c
Vikas Kumar <88340387+Vikas2489@users.noreply.github.com>
2022-01-11 01:08:13 +0530

    completed

--- block-BJaabd/code/index.js
+++ block-BJaabd/code/index.js
@@ -22,13 +22,25 @@ n = 7 => output: heeeeeeello
 👇
 */
 // [Your code goes here]
-
+let count = +prompt(`Enter the number of times you want that e in hello!`);
+let mid = "";
+let start = "h",
+    end = "llo";
+for (i = 0; i < count; i++) {
+    mid += "e"
+}
+alert(start + mid + end);
 /*
 Program to calculate the sum of first n natural numbers(1,2,3...n are known as natural numbers). Prompt user to enter n (using `prompt`) then based on input provided calculate and show result in `alert`.
 👇
 */
 // [Your code goes here]
-
+let num = +prompt("enter any value!");
+let sum = 0;
+for (i = 1; i <= num; i++) {
+    sum += i;
+}
+alert(sum);
 /* Switch Statement
 
  🎖Using switch statement do the following
@@ -47,6 +59,39 @@ Take a number value from user and alert the message if it matches the conditions
 
 */
 // [Your code goes here]
+let numberFromUser = +prompt("Enter a number?");
+
+switch (numberFromUser) {
+    case 1:
+        alert("ONE");
+        break;
+    case 2:
+        alert("TWO");
+        break;
+    case 3:
+        alert("THREE");
+        break;
+    case 4:
+        alert("FOUR");
+        break;
+    case 5:
+        alert("FIVE");
+        break;
+    case 6:
+        alert("SIX");
+        break;
+    case 7:
+        alert("SEVEN");
+        break;
+    case 8:
+        alert("EIGHT");
+        break;
+    case 9:
+        alert("NINE");
+        break;
+    default:
+        alert("please try again");
+}
 
 /*
 🎖Using switch statement do the following
@@ -63,16 +108,61 @@ Take the value of `marks` (0-100) from user using `prompt` and `alert` the messa
 */
 
 // [Your code goes here]
+let marks = +prompt("Enter your marks from 0-100?");
+switch (true) {
+    case marks > 90:
+        alert("Grade AA");
+        break;
+    case marks > 80 && marks <= 90:
+        alert("Grade AB");
+        break;
+    case marks > 70 && marks <= 80:
+        alert("Grade BB");
+        break;
+    case marks > 60 && marks <= 70:
+        alert("Grade BC");
+        break;
+    case marks > 50 && marks <= 60:
+        alert("Grade CC");
+        break;
+    case marks > 40 && marks <= 50:
+        alert("Grade CD");
+        break;
+    case marks > 30 && marks <= 40:
+        alert("Grade DD");
+        break;
+    case marks <= 30:
+        alert("Grade FF");
+        break;
+    default:
+        alert("enter a valid input!");
+        break;
+}
 
 /*
  🎖Write a JavaScript program that takes two `integers` from user (using prompt) and alerts the larger number.
 */
 // [your code goes here]
+let num1 = +prompt("enter your first integer");
+let num2 = +prompt("enter your second integer");
+
+if (num1 > num2) { alert(`${num1} is greater than ${num2}`); } else { alert(`${num2} is greater than ${num1}`); }
 
 /*
 🎖Write a JavaScript conditional statement to find the sign (+, -) of product of three numbers. Take those three numbers from user using `prompt`. Display an alert box with the specified sign.
 */
 // [Your code goes here]
+let numOne = +prompt("enter your first number");
+let numTwo = +prompt("enter your second number");
+let numThree = +prompt("enter your third number");
+
+let product = numOne * numTwo * numThree;
+
+if (product > 0) {
+    alert("the value is positive");
+} else {
+    alert("the value is negative");
+}
 
 /* Calculator
  🎖Make a simple calculator with these functions. Using prompt, type conversion, if else statement. Use prompt to take the input from user i.e two numbers and an operation (Add, Sub, Mul, Div).
@@ -87,3 +177,38 @@ Take the value of `marks` (0-100) from user using `prompt` and `alert` the messa
 */
 
 // [Your code goes here]
+let numOnee = +prompt("enter your first number");
+let numTwoo = +prompt("enter your second number");
+let operator = prompt(`Enter any operator add,div,sub,multiplication`);
+
+switch (operator) {
+    case "add":
+        alert(`sum is ${numOnee+ numTwoo}`);
+        break;
+    case "sub":
+        alert(`sub is ${numOnee- numTwoo}`);
+        break;
+    case "multiplication":
+        alert(`mmultiplicaiton is ${numOnee* numTwoo}`);
+        break;
+    case "div":
+        alert(`divison is ${numOnee/ numTwoo}`);
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
